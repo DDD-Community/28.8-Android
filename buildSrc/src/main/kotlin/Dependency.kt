47a4709d2fab834e880b53dee4db783795579270
jhp-dev <jhp0415.dev@gmail.com>
2022-11-18 10:28:02 +0900

    [#3] : 코드 정렬

--- buildSrc/src/main/kotlin/Dependency.kt
+++ buildSrc/src/main/kotlin/Dependency.kt
@@ -8,13 +8,13 @@ object Dependency {
 
     object Kotlin {
         const val version = "1.7.0"
-        const val KOTLIN_STDLIB      = "org.jetbrains.kotlin:kotlin-stdlib:${version}"
+        const val KOTLIN_STDLIB = "org.jetbrains.kotlin:kotlin-stdlib:${version}"
     }
 
     object AndroidX {
         const val CORE = "androidx.core:core-ktx:1.8.0"
         const val APPCOMPAT = "androidx.appcompat:appcompat:1.5.0"
-        const val CONSTRAINT_LAYOUT     = "androidx.constraintlayout:constraintlayout:2.1.4"
+        const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.4"
 
         object Compose {
             private const val version = "1.2.0"
@@ -27,31 +27,31 @@ object Dependency {
 
         object Lifecycle {
             private const val version = "2.5.1"
-            const val COMPOSE     = "androidx.lifecycle:lifecycle-viewmodel-compose:$version"
+            const val COMPOSE = "androidx.lifecycle:lifecycle-viewmodel-compose:$version"
         }
 
         object Navigation {
             private const val version = "2.5.2"
-            const val COMPOSE   = "androidx.navigation:navigation-compose:${version}"
+            const val COMPOSE = "androidx.navigation:navigation-compose:${version}"
         }
     }
 
     object Hilt {
         const val version = "2.42"
-        const val HILT           = "com.google.dagger:hilt-android:${version}"
-        const val HILT_COMPILER  = "com.google.dagger:hilt-android-compiler:${version}"
+        const val HILT = "com.google.dagger:hilt-android:${version}"
+        const val HILT_COMPILER = "com.google.dagger:hilt-android-compiler:${version}"
     }
 
     object Retrofit {
         private const val version = "2.9.0"
-        const val RETROFIT          = "com.squareup.retrofit2:retrofit:${version}"
-        const val CONVERTER_GSON    = "com.squareup.retrofit2:converter-gson:${version}"
+        const val RETROFIT = "com.squareup.retrofit2:retrofit:${version}"
+        const val CONVERTER_GSON = "com.squareup.retrofit2:converter-gson:${version}"
     }
 
     object OkHttp {
         private const val version = "4.9.1"
-        const val OKHTTP                = "com.squareup.okhttp3:okhttp:${version}"
-        const val LOGGING_INTERCEPTOR   = "com.squareup.okhttp3:logging-interceptor:${version}"
+        const val OKHTTP = "com.squareup.okhttp3:okhttp:${version}"
+        const val LOGGING_INTERCEPTOR = "com.squareup.okhttp3:logging-interceptor:${version}"
     }
 
     object Google {
@@ -60,13 +60,13 @@ object Dependency {
 
     object JunitTest {
         const val version = "4.13.2"
-        const val JUNIT                 = "junit:junit:${version}"
-        const val ANDROID_JUNIT_RUNNER  = "AndroidJUnitRunner"
+        const val JUNIT = "junit:junit:${version}"
+        const val ANDROID_JUNIT_RUNNER = "AndroidJUnitRunner"
     }
 
     object AndroidXTest {
-        const val ANDROID_JUNIT     = "androidx.test.ext:junit:1.1.3"
-        const val TEST_RUNNER       = "androidx.test:runner:1.4.0"
-        const val ESPRESSO_CORE     = "androidx.test.espresso:espresso-core:3.4.0"
+        const val ANDROID_JUNIT = "androidx.test.ext:junit:1.1.3"
+        const val TEST_RUNNER = "androidx.test:runner:1.4.0"
+        const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:3.4.0"
     }
 }
