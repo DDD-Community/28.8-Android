73f20ca828da539155f54ac4c1e5878e6cd06dda
= <ruddns8213@naver.com>
2022-12-03 20:56:30 +0900

    #2 : 디자인 시스템 appbar, typo, color set 정리

---
+++ core-designsystem/src/main/java/com/ddd/carssok/core/designsystem/Typo.kt
@@ -0,0 +1,18 @@
+package com.ddd.carssok.core.designsystem
+
+import androidx.compose.ui.text.font.FontWeight
+
+enum class TypoStyle(val textSize: Int, val fontWeight: FontWeight) {
+    DISPLAY_LARGE_32(32, FontWeight.Bold),
+    DISPLAY_MEDIUM_28(28, FontWeight.Bold),
+    DISPLAY_SMALL_24(24, FontWeight.Bold),
+    HEADLINE_LARGE_20(20, FontWeight.Bold),
+    HEADLINE_MEDIUM_18(18, FontWeight.Bold),
+    HEADLINE_SMALL_16(16, FontWeight.Normal),
+    HEADLINE_X_SMALL_14(14, FontWeight.Normal),
+
+    BODY_LARGE_16(16, FontWeight.Normal),
+    BODY_MEDIUM_14(14, FontWeight.Normal),
+    BODY_SMALL_12(12, FontWeight.Normal),
+    BODY_X11_SMALL(11, FontWeight.Normal),
+}
