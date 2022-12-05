0f4041a5937221828ce8d91dfd83f04e8a99208f
jhp-dev <jhp0415.dev@gmail.com>
2022-12-06 01:13:56 +0900

    [#10] 주석추가

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,7 @@ repositories {
 }
 
 dependencies {
+    // TODO jhp-dev : custom plugin 정의를 위해 추가, plugin version 관리 일원화
     implementation("com.android.tools.build:gradle:7.3.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     implementation("com.google.dagger:hilt-android-gradle-plugin:2.42")
